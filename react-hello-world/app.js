7333b73b1ba1d4cecf88c35ef9fd4d7e168727cc
malikasinger1 <malikasinger1@gmail.com>
2017-02-01 23:35:47 +0500

    hello world

--- react-hello-world/app.js
+++ react-hello-world/app.js
@@ -1,1 +1,13 @@
-ReactDOM.render(  React.DOM.h1(null, "hello world")   , document.getElementById("root"));
+ReactDOM.render(
+
+    React.DOM.h1(null, "hello world")
+
+    , document.getElementById("root"));
+
+
+
+ReactDOM.render(
+
+    React.DOM.h1({ id: "alibhai", className: "abc" }, "hello world1")
+
+    , document.getElementById("root1"));

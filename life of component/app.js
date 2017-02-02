ac4f42b6df692cc33f509e6d40613aae650dcc72
malikasinger1 <malikasinger1@gmail.com>
2017-02-02 14:24:52 +0500

    lifecycle

--- life of component/app.js
+++ life of component/app.js
@@ -81,5 +81,63 @@ ReactDOM.render(
     React.createElement(textAreaCounter, { text: "ali bhai" })
     , document.getElementById("root2"));
 
+//---------------------------------------------------------------------
+//lifecycle
+var textAreaCounter = React.createClass({
+    _log: function (methodName, args) {
+        console.log(methodName, args);
+    },
+    componentWillUpdate: function () {
+        this._log('componentWillUpdate', arguments);
+    },
+    componentDidUpdate: function (oldProps, oldState) {
+        this._log('componentDidUpdate', arguments);
+        if (this.state.text.length > 10) {
+            this.replaceState(oldState);
+        }
+    },
+    componentWillMount: function () {
+        this._log('componentWillMount', arguments);
+    },
+    componentDidMount: function () {
+        this._log('componentDidMount', arguments);
+    },
+    componentWillUnmount: function () {
+        this._log('componentWillUnmount', arguments);
+    },
+    propType: {
+        text: React.PropTypes.string
+    },
+    getDefaultProps: function () {
+        return {
+            text: 'not provided',
+        };
+    },
+    getInitialState: function () {
+        return {
+            text: this.props.text,
+        }
+    },
+    _textChange: function (ev) {
+        this.setState({
+            text: ev.target.value,
+        });
+    },
+    render: function () {
+        return React.DOM.div(null,
+            React.DOM.textarea({
+                value: this.state.text,
+                onChange: this._textChange,
+            }),
+            React.DOM.h3(null, "text count: " + this.state.text.length),
+            React.DOM.h3(null, "text: " + this.state.text)
+        );
+    }
+});
+
+ReactDOM.render(
+    React.createElement(textAreaCounter, { text: "ali bhai" })
+    , document.getElementById("root3"));
+
 
 

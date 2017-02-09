b6f13862a76f01ad7ef952444e4d3c596195a29a
malikasinger1 <malikasinger1@gmail.com>
2017-02-10 01:42:53 +0500

    hello world modified

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -5,20 +5,46 @@ import './index.css';
 
 
 
-let h1 = React.createElement('h1', null, 'Hello world!');
+// let h1 = React.createElement('h1', null, 'Hello world!');
+// class HelloWorld extends React.Component {
+//     render() {
+//         return React.createElement('div', null, h1, h1)
+//     }
+// }
 
+
+//this is a Component class
 class HelloWorld extends React.Component {
     render() {
-        return React.createElement('div', null, h1, h1)
+        return React.createElement('h1', null, "Hello " + this.props.framworkName + " world")
+
     }
 }
 
+//making three seaprate instances of class with different props
+let a = React.createElement(HelloWorld, {
+    id: 'ember',
+    framworkName: 'Ember.js',
+    title: 'a framwork'
+});
 
+let b = React.createElement(HelloWorld, {
+    id: 'ember1',
+    framworkName: 'Angular',
+    title: 'a framwork'
+})
+let c = React.createElement(HelloWorld, {
+    id: 'ember1',
+    framworkName: 'React',
+    title: 'a framwork'
+})
 
+//display that instances as chid of a div
 ReactDOM.render(
-    React.createElement(HelloWorld, null),
+    React.createElement('div', null, a, b, c),
     document.getElementById('root')
 )
+
 
 
 

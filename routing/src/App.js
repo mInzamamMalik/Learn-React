287032481a66cf894d8b06dc69e48efcb3af1c0e
malikasinger1 <malikasinger1@gmail.com>
2017-02-10 17:44:52 +0500

    react cli boilerplate

---
+++ routing/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Menu extends Component {
+    render() {
+        let menus = [
+            'Home',
+            'About',
+            'Services',
+            'Portfolio',
+            'Contact us'
+        ];
+        return React.createElement('div',
+            null,
+            menus.map((v, i) => {
+                return React.createElement('div',
+                    { key: i },
+                    React.createElement(Link, { label: v })
+                )
+            })
+        )
+    }
+}
+export default App;

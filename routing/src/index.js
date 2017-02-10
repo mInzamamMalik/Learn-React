2f70285327bc3ad6b47600f5356ca491607dd9c8
malikasinger1 <malikasinger1@gmail.com>
2017-02-10 18:23:53 +0500

    Basic routing done

--- routing/src/index.js
+++ routing/src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Router from './router.jsx';
+import logo from './logo.svg';
+
+const mapping = {
+    '#profile': <div >This is profile page (<a href="#">home</a>)</div>,
+    '#contacts': <div >This is Contacts page (<a href="#">home</a>)</div>,
+    '#about': <div >This is About Us page (<a href="#">home</a>)</div>,
+    '*': <div>
+        <h1>This is Dashboard </h1>
+        <br />
+        <a href="#profile">Profile</a>
+        <br />
+        <a href="#contacts">Contacts</a>
+        <br />
+        <a href="#about">About</a>
+    </div>
+}
 
 ReactDOM.render(
-    <App />,
+    <Router mapping={mapping} />,
     document.getElementById('content')
-);
+)

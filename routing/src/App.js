3cfb551fe68369d5d9f704c62b1ad6bd7c233d8c
malikasinger1 <malikasinger1@gmail.com>
2017-02-10 17:48:19 +0500

    boiler plate bug fix

--- routing/src/App.js
+++ routing/src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-class Menu extends Component {
+class App extends Component {
     render() {
         let menus = [
             'Home',
@@ -11,15 +10,7 @@ class Menu extends Component {
             'Portfolio',
             'Contact us'
         ];
-        return React.createElement('div',
-            null,
-            menus.map((v, i) => {
-                return React.createElement('div',
-                    { key: i },
-                    React.createElement(Link, { label: v })
-                )
-            })
-        )
+        return <div>gsfg</div>
     }
 }
 export default App;
--- routing/src/index.js
+++ routing/src/index.js
@@ -1,59 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Menu from './App';
+import App from './App';
 import './index.css';
 
-
-// //state 1:
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById('root')
-// );
-
-
-
-// //state 2:
-// let h1 = React.createElement('h1', null, 'Hello world!');
-// class HelloWorld extends React.Component {
-//     render() {
-//         return React.createElement('div', null, h1, h1)
-//     }
-// }
-// ReactDOM.render(
-// React.createElement('div', null, h1, h1, h1),
-//     document.getElementById('root')
-// )
-
-
-// //state 3:
-// //this is a Component class
-// class HelloWorld extends React.Component {
-//     render() {
-//         return React.createElement('h1', null, "Hello " + this.props.framworkName + " world")
-
-//     }
-// }
-
-// //making three seaprate instances of class with different props
-// let a = React.createElement(HelloWorld, {
-//     id: 'ember',
-//     framworkName: 'Ember.js',
-//     title: 'a framwork'
-// });
-
-// let b = React.createElement(HelloWorld, {
-//     id: 'ember1',
-//     framworkName: 'Angular',
-//     title: 'a framwork'
-// })
-// let c = React.createElement(HelloWorld, {
-//     id: 'ember1',
-//     framworkName: 'React',
-//     title: 'a framwork'
-// })
-
-// //display that instances as chid of a div
-// ReactDOM.render(
-//     React.createElement('div', null, a, b, c),
-//     document.getElementById('root')
-// )
+ReactDOM.render(
+    <App />,
+    document.getElementById('content')
+);

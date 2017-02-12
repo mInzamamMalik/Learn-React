6986c3dc7bcebab3faee417bc0ded1a7ea0bf60d
Inzamam Malik <malikasinger1@gmail.com>
2017-02-12 11:19:34 +0500

    boilerplate bug fix

--- project6-autocomplete/src/App.js
+++ project6-autocomplete/src/App.js
@@ -3,27 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 
-
-
-
-
-
-class Menu extends Component {
+class App extends Component {
     render() {
-        let menus = ['Home',
-            'About',
-            'Services',
-            'Portfolio',
-            'Contact us']
-        return React.createElement('div',
-            null,
-            menus.map((v, i) => {
-                return React.createElement('div',
-                    { key: i },
-                    React.createElement(Link, { label: v })
-                )
-            })
-        )
+
+        return <div>hello world</div>
     }
 }
-export default Menu;
+export default App;

551ec1336c572bbbe80081294887b76eaf0dc4b2
Inzamam Malik <malikasinger1@gmail.com>
2017-02-12 13:16:27 +0500

    bootstrap added

--- project6-autocomplete/src/App.js
+++ project6-autocomplete/src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import { Navbar, Jumbotron, Button, Panel } from 'react-bootstrap';
+
 
 class App extends Component {
     render() {
 
-        return <div>hello world</div>
+        return <Panel header="React.js" bsStyle="primary"> Hello World </Panel>
     }
 }
 export default App;

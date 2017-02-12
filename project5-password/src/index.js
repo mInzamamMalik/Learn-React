1843b1f488fc49baca9c37f76cd5273b2adb33f6
Inzamam Malik <malikasinger1@gmail.com>
2017-02-12 11:15:12 +0500

    bootstrap added

--- project5-password/src/index.js
+++ project5-password/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')

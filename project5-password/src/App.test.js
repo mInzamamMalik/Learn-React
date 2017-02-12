d0a0a0cab039a1ede5233d6c56a7a22f5352d17c
Inzamam Malik <malikasinger1@gmail.com>
2017-02-12 20:49:30 +0500

    looks like working but still behaving odd

--- project5-password/src/App.test.js
+++ project5-password/src/App.test.js
@@ -3,23 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-const generatePassword = require('../js/generate-password.js')
-const pattern = /^[A-Za-z0-9\!\@\#\$\%\^\&\*\(\)\_\+\{\}\:\"\<\>\?\\|\[\]\/'\,\.\`\~]{8,16}$/
-let password, password2
-
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
 });
 
-it('returns a generated password of the set pattern', () => {
-    password = generatePassword()
-    expect(password).toMatch(pattern)
-})
+var sum = function (a, b) {
+    return a + b;
+}
 
-it('return a new value different from the previous one', () => {
-    password2 = generatePassword()
-    expect(password2).toMatch(pattern)
-    expect(password2).not.toEqual(password)
-})
-
+it('testing function', () => {
+    expect(sum(1, 2)).toBe(3);
+    expect(sum("5", 4)).toBe("54");    
+});

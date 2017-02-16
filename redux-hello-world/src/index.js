a85cf8eef025c0b76f5946e347430bc679bc9986
Inzamam Malik <malikasinger1@gmail.com>
2017-02-16 20:42:08 +0500

    increment is up and running

--- redux-hello-world/src/index.js
+++ redux-hello-world/src/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import Counter from './components/counter.jsx'
+import counter from './store/reducers.js'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const store = createStore(counter)
+const rootEl = document.getElementById('root')
+
+const render = () => ReactDOM.render(
+  <Counter
+    value={store.getState()}
+    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+  />,
+  rootEl
+)
+
+render()
+store.subscribe(render)

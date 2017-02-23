84811fc47365f20b44396bc5d287bafa7bed0818
Inzamam Malik <malikasinger1@gmail.com>
2017-02-23 23:03:05 +0500

    store is connected to app

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import store from './store/store'
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
 );

b6e3ab1eb470e8e5652f3968bfe3357ee52715a8
Inzamam Malik <malikasinger1@gmail.com>
2017-02-25 15:11:04 +0500

    redux dev tool configured

--- redux-todo-app/src/store/store.js
+++ redux-todo-app/src/store/store.js
@@ -2,5 +2,8 @@ import { createStore } from 'redux'
 
 import allReducers from './reducers/index.js'
 
-const store = createStore(allReducers)
+const store = createStore(
+    allReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 export default store;

9e7275d3f5b8b3ceca6ce97c6dc66034b072b1ce
Inzamam Malik <malikasinger1@gmail.com>
2017-03-02 21:55:24 +0500

    store is connected to components

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import store from './store/store'
+
+import GitUser from './components/user';
+
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <GitUser />
+    </Provider>
+    , document.getElementById('root')
 );

0743cebaeab8e69a38bc28413c0a9fc3efaaa314
Inzamam Malik <malikasinger@gmail.com>
2017-03-07 16:42:03 +0500

    CODE IN MINIMAL NOW

--- redux-middlewarw-firebase-helloworld/src/store/epic/auth.js
+++ redux-middlewarw-firebase-helloworld/src/store/epic/auth.js
@@ -10,39 +10,4 @@ export class AuthEpic {
             AuthEpic.registerUserOnFirebase(dispatch, credentials);
         }
     }
-
-    // static registerUserOnFirebase(dispatch, credentials) {
-    //     firebase.auth()
-    //         .createUserWithEmailAndPassword(credentials.email, credentials.password)
-    //         .then(function (authUser) {
-    //             console.log("signup successfull ", authUser);
-    //             AuthEpic.createUserInFirebase(dispatch, credentials, authUser);
-    //         })
-    //         .catch(function (error) {
-    //             //var errorCode = error.code;
-    //             //var errorMessage = error.message;
-    //             console.log("signup error ", error);
-    //             dispatch(AuthAction.signupRejected(error));
-    //         });
-
-    // }
-
-    // // Signin Functions Starts
-    // static signin(credentials) {
-    //     console.log("test ", credentials);
-    //     return (dispatch) => {
-    //         dispatch(AuthAction.signin())
-    //         AuthEpic.authenticateUserOnFirebase(dispatch, credentials);
-    //     }
-    // }
-
-    // static getUserFromFirebase(dispatch, authUser) {
-    //     firebase.database().ref('/')
-    //         .child(`users/${authUser.uid}`)
-    //         .once('value')
-    //         .then(function (userObj) {
-    //             console.log("user after login ", userObj.val());
-    //             dispatch(AuthAction.signinSuccessful(userObj.val()));
-    //         });
-    // }
 }

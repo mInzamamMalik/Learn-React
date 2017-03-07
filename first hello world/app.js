944a8bf6e071eec8f924b5f53c821760f2746d53
Inzamam Malik <malikasinger@gmail.com>
2017-03-07 21:53:20 +0500

    very first hello world of react

---
+++ first hello world/app.js
@@ -0,0 +1,12 @@
+ReactDOM.render(
+
+
+    React.createElement('div', null,
+
+        React.createElement('h1', null, "hello world"),
+        React.createElement('h1', null, "hello world")
+
+    )
+   
+
+    , document.getElementById('root'))

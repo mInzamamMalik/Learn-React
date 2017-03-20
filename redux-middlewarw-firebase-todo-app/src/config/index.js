e829c0f03bf019dd19b31f25a2142c84fe1231c7
Inzamam Malik <malikasinger1@gmail.com>
2017-03-20 21:02:41 +0500

    rename

---
+++ redux-middlewarw-firebase-todo-app/src/config/index.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase'
+
+//malikasinger1@gmail
+var firebaseConfig = {
+    apiKey: "AIzaSyDspeNWtv6xycKzfsFA2mcVWxIYaKA1Mkk",
+    authDomain: "test-project-5a3f4.firebaseapp.com",
+    databaseURL: "https://test-project-5a3f4.firebaseio.com",
+    storageBucket: "test-project-5a3f4.appspot.com",
+    messagingSenderId: "523897901107"
+}
+firebase.initializeApp(firebaseConfig);

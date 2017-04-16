702a70f782082695e1121bbe1bf92aae7b03d84e
Inzamam Malik <malikasinger1@gmail.com>
2017-04-17 02:34:55 +0500

    table added for client

--- redux-middleware-firebase-auth/src/components/profile.js
+++ redux-middleware-firebase-auth/src/components/profile.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { AuthActions } from './../store/action/auth';
 import { RaisedButton, TextField } from 'material-ui';
+import { Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn }
+    from 'material-ui/Table';
 
 function mapStateToProps(state) {
     return {
@@ -13,10 +15,76 @@ function mapDispatchToProps(dispatch) {
         logout: () => dispatch(AuthActions.logout())
     };
 }
+const styles = {
+    propContainer: {
+        width: 200,
+        overflow: 'hidden',
+        margin: '20px auto 0',
+    },
+    propToggleHeader: {
+        margin: '20px auto 10px',
+    },
+};
+
+const tableData = [
+    {
+        name: 'John Smith',
+        status: 'Employed',
+        selected: true,
+    },
+    {
+        name: 'Randal White',
+        status: 'Unemployed',
+    },
+    {
+        name: 'Stephanie Sanders',
+        status: 'Employed',
+        selected: true,
+    },
+    {
+        name: 'Steve Brown',
+        status: 'Employed',
+    },
+    {
+        name: 'Joyce Whitten',
+        status: 'Employed',
+    },
+    {
+        name: 'Samuel Roberts',
+        status: 'Employed',
+    },
+    {
+        name: 'Adam Moore',
+        status: 'Employed',
+    },
+];
 class Profile extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            fixedHeader: true,
+            fixedFooter: true,
+            stripedRows: false,
+            showRowHover: false,
+            selectable: true,
+            multiSelectable: false,
+            enableSelectAll: false,
+            deselectOnClickaway: true,
+            showCheckboxes: true,
+            height: '300px',
+        };
     }
+    handleToggle = (event, toggled) => {
+        this.setState({
+            [event.target.name]: toggled,
+        });
+    };
+
+    handleChange = (event) => {
+        this.setState({ height: event.target.value });
+    };
+
+
 
     render() {
         return (
@@ -24,6 +92,63 @@ class Profile extends Component {
                 this is profile page
 
                 <RaisedButton onClick={() => { this.props.logout() }}> Logout</RaisedButton>
+
+
+                <Table
+                    height={this.state.height}
+                    fixedHeader={this.state.fixedHeader}
+                    fixedFooter={this.state.fixedFooter}
+                    selectable={this.state.selectable}
+                    multiSelectable={this.state.multiSelectable}
+                >
+                    <TableHeader
+                        displaySelectAll={this.state.showCheckboxes}
+                        adjustForCheckbox={this.state.showCheckboxes}
+                        enableSelectAll={this.state.enableSelectAll}
+                    >
+                        <TableRow>
+                            <TableHeaderColumn colSpan="3" tooltip="Super Header" style={{ textAlign: 'center' }}>
+                                Super Header
+              </TableHeaderColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableHeaderColumn tooltip="The ID">ID</TableHeaderColumn>
+                            <TableHeaderColumn tooltip="The Name">Name</TableHeaderColumn>
+                            <TableHeaderColumn tooltip="The Status">Status</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody
+                        displayRowCheckbox={this.state.showCheckboxes}
+                        deselectOnClickaway={this.state.deselectOnClickaway}
+                        showRowHover={this.state.showRowHover}
+                        stripedRows={this.state.stripedRows}
+                    >
+                        {tableData.map((row, index) => (
+                            <TableRow key={index} selected={row.selected}>
+                                <TableRowColumn>{index}</TableRowColumn>
+                                <TableRowColumn>{row.name}</TableRowColumn>
+                                <TableRowColumn>{row.status}</TableRowColumn>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                    <TableFooter
+                        adjustForCheckbox={this.state.showCheckboxes}
+                    >
+                        <TableRow>
+                            <TableRowColumn>ID</TableRowColumn>
+                            <TableRowColumn>Name</TableRowColumn>
+                            <TableRowColumn>Status</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn colSpan="3" style={{ textAlign: 'center' }}>
+                                Super Footer
+              </TableRowColumn>
+                        </TableRow>
+                    </TableFooter>
+                </Table>
+
+
+
 
             </div>
 

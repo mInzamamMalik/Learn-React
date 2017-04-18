7af63b28122f631c1800ba5ba07b320c5155f6eb
Inzamam Malik <malikasinger1@gmail.com>
2017-04-18 20:45:05 +0500

    client firebase account connected

--- redux-middleware-firebase-auth/src/config/index.js
+++ redux-middleware-firebase-auth/src/config/index.js
@@ -1,11 +1,11 @@
 import * as firebase from 'firebase'
 
-//malikasinger1@gmail
 var firebaseConfig = {
-    apiKey: "AIzaSyDspeNWtv6xycKzfsFA2mcVWxIYaKA1Mkk",
-    authDomain: "test-project-5a3f4.firebaseapp.com",
-    databaseURL: "https://test-project-5a3f4.firebaseio.com",
-    storageBucket: "test-project-5a3f4.appspot.com",
-    messagingSenderId: "523897901107"
+    apiKey: "AIzaSyCrJU9HKS83_4OckJBgULPqLHOdyziejl8",
+    authDomain: "webapp-34625.firebaseapp.com",
+    databaseURL: "https://webapp-34625.firebaseio.com",
+    projectId: "webapp-34625",
+    storageBucket: "webapp-34625.appspot.com",
+    messagingSenderId: "1029739602821"
 }
 firebase.initializeApp(firebaseConfig);
